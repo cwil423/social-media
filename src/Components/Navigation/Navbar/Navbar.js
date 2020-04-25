f2cc13604aea2b59980f22d501464f6b799541de
Cole Williamson <williamsoncole@gmail.com>
2020-04-25 15:39:56 -0500

    UI changes and made app device responsive

--- src/Components/Navigation/Navbar/Navbar.js
+++ src/Components/Navigation/Navbar/Navbar.js
@@ -12,13 +12,13 @@ const Navbar = () => {
     <div className={classes.navbar}>
       <h1 className={classes.title}>FriendBook</h1>
       <ul className={classes.navLinks}>
-        <NavLink className={classes.navLink} to='/home_page'>
+        <NavLink className={classes.navLink} activeClassName={classes.isActive} to='/home_page'>
           Home
         </NavLink>
-        <NavLink className={classes.navLink} to='/create_post'>
+        <NavLink className={classes.navLink} activeClassName={classes.isActive} to='/create_post'>
           Create Post
         </NavLink>
-        <NavLink className={classes.navLink} to='profile'>
+        <NavLink className={classes.navLink} activeClassName={classes.isActive} to='profile'>
           Profile
         </NavLink>
         <NavLink className={classes.navLink} to='/login' onClick={signOutHandler}>
